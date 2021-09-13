1e5fccbadc3e14b91ca8ba509f8dcc5ae930654e
Ryan <65420906+Ryan-Webb62@users.noreply.github.com>
2021-09-12 21:16:14 -0400

    test

--- src/js/config.js
+++
@@ -1,4 +0,0 @@
-export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes`;
-// 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcd09'
-export const TIMEOUT_SEC = 10;
-// this is new stuff
